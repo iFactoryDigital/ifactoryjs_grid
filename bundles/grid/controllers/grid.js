8cc6c25ccee33b777e2296f295c0f22ed50fd039
EdenCoder <alex@adlist.co.nz>
2018-11-01 13:34:39 +1000

    add edenjs grid controller

---
+++ bundles/grid/controllers/grid.js
@@ -0,0 +1,26 @@
+// Use strict
+
+// Require dependencies
+const Controller = require('controller');
+
+/**
+ * Build Grid Controller
+ */
+class GridController extends Controller {
+
+  /**
+   * Construct example controller class
+   */
+  constructor () {
+    // Run super
+    super();
+
+  }
+}
+
+/**
+ * Export Grid Controller
+ *
+ * @type {GridController}
+ */
+exports = module.exports = GridController;
